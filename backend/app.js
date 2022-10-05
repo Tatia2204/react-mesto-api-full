bcf21486e6eb02a6d4569e8bfc7b7d06822ee196
Tania <ts2204@mail.ru>
2022-10-05 22:00:39 +0700

    Проектная работа 15 (2)

--- backend/app.js
+++ backend/app.js
@@ -15,9 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: 'https://tanja2204.nomoredomains.icu',
-}));
+app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
