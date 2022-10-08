25772ab72836f14b6251233d8109499dbbbb1c4c
Tania <ts2204@mail.ru>
2022-10-08 17:38:31 +0700

    Проектная работа 15 (2)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
 const { AuthError } = require('../errors/AuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  const { JWT_SECRET = 'tts' } = process.env;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthError('Необходима авторизация');
@@ -13,7 +14,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'tts');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
