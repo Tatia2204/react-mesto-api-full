2aa96b77308177ea5bbaf570adfd0e8b69e12d3d
Tania <ts2204@mail.ru>
2022-10-09 20:01:55 +0700

    Проектная работа 15 (2)

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,35 +1,35 @@
 import React, { useContext } from "react";
 import { CurrentUser } from "../contexts/CurrentUser.js";
 
-function Card({card, onCardClick, onCardLike, onCardClickDelete, onCardDelete}) {
+function Card(props) {
 
     const currentUser = useContext(CurrentUser);
 
     function handleClick () {
-        onCardClick(card);
+        props.onCardClick(props.card);
     }
 
     function handleLikeClick () {
-        onCardLike(card);
+        props.onCardLike(props.card);
     }
 
     function handleDeleteClick () {
-        onCardClickDelete(card._id);
+        props.onCardClickDelete(props.card._id);
     }
 
     function handleDeleteCard () {
-        onCardDelete(card._id);
+        props.onCardDelete(props.card._id);
     }
 
     // Определяем, являемся ли мы владельцем текущей карточки
-    const isOwn = card.owner === currentUser._id;
+    const isOwn = props.card.owner === currentUser._id;
 
     const cardDeleteButtonClassName = (
         `element__remove ${isOwn ? 'element__remove_visible' : 'element__remove_hidden'}`
     );
 
     //есть ли у карточки лайк
-    const isLiked = card.likes.some(i => i === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
 
 
     const cardLikeButtonClassName = `element__like ${isLiked ? 'element__like_active' : ''}`;
@@ -39,13 +39,13 @@ function Card({card, onCardClick, onCardLike, onCardClickDelete, onCardDelete}) 
             <button aria-label="Удалить карточку" className={cardDeleteButtonClassName} type="button"
                     onClick={handleDeleteClick}
                     onClick={handleDeleteCard}/>
-            <img src={card.link} alt={card.name} className="element__mask-group" onClick={handleClick}/>
+            <img src={props.card.link} alt={props.card.name} className="element__mask-group" onClick={handleClick}/>
             <div className="element__group">
-                <h2 className="element__location">{card.name}</h2>
+                <h2 className="element__location">{props.card.name}</h2>
                 <div className="element__like-group">
                     <button aria-label="Отметить фото" className={cardLikeButtonClassName} type="button"
                             onClick={handleLikeClick}/>
-                    <p className="element__likes">{card.likes.length}</p>
+                    <p className="element__likes">{props.card.likes.length}</p>
                 </div>
             </div>
         </article>
